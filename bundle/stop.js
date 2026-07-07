ad3b50dd711976fd6f1bfbfa9a6ff9949838f90a
Tat Dat Duong <david@duong.cz>
2026-07-07 15:29:17 +0200

    release: 0.3.0

--- bundle/stop.js
+++ bundle/stop.js
@@ -666,7 +666,7 @@ var DEFAULT_TAGS = ["cursor", "coding-agent"];
 var DEFAULT_PROJECT = "cursor";
 
 // dist/config.js
-var LS_INTEGRATION_VERSION = true ? "0.2.0" : process.env.LANGSMITH_CURSOR_INTEGRATION_VERSION || void 0;
+var LS_INTEGRATION_VERSION = true ? "0.3.0" : process.env.LANGSMITH_CURSOR_INTEGRATION_VERSION || void 0;
 var PROVIDER_HOSTS = {
   github: "github.com",
   gitlab: "gitlab.com",
